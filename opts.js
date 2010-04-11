923ead4b7d2ed3544dde4a44379bf1fab9b61160
Kay O <khtdr@users.noreply.github.com>
2010-04-11 01:51:57 -0600

    support for arguments and namespaces

--- opts.js
+++ opts.js
@@ -36,24 +36,41 @@ either expressed or implied, of Joey Mazzarelli.
 
 var puts        = require('sys').puts
   , values      = {}
-  , args        = []
+  , args        = {}
+  , argv        = []
   , errors      = []
-  , descriptors = [];
+  , descriptors = {opts:[], args:[]};
 
+/**
+ * Add a set of option descriptors, not yet ready to be parsed.
+ * See exports.parse for description of options object
+ *
+ * Additionally, it takes a namespace as an argument, useful for
+ * building options for a library in addition to the main app.
+ */
+exports.add = function (options, namespace) {
+  for (var i=0; i<options.length; i++) {
+    options[i].namespace = namespace;
+    descriptors.opts.push(options[i]);
+  }
+};
 
 /**
  * Parse the command line options
- * @param array options Options to parse
+ * @param array options  Options to parse
+ * @param array args     Arguments to parse
+ * @param bool  help     Automatically generate help message, default false
  *
+ * ===== Options Docs =====
  * Each option in the array can have the following fields. None are required, 
  * but you should at least provide a short or long name.
  *   {
  *     short       : 'l',
  *     long        : 'list',
  *     description : 'Show a list',
- *     value       : false,
- *     required    : true,
- *     callback    : function (value) { ... }
+ *     value       : false,  // default false
+ *     required    : true,   // default false
+ *     callback    : function (value) { ... },
  *   }
  *
  * You can add an automatically generated help message by passing
@@ -63,8 +80,28 @@ var puts        = require('sys').puts
  *     description : 'Show this help message',
  *     callback    : require('./opts').help,
  *   }
+ *
+ * ===== Arguments Docs =====
+ * Arguments are different than options, and simpler. They typically come 
+ * after the options, but the library really doesn't care. Each argument
+ * can have the form of:
+ *   {
+ *     name     : 'script',
+ *     required : true,      // default false
+ *     callback : function (value) { ... },
+ *   }
  */
-exports.parse = function (options, help) {
+exports.parse = function (options, params, help) {
+
+  if (params === true) {
+    help == true;
+  } else if (!params) {
+    params = [];
+  } else {
+    for (var i=0; i<params.length; i++) {
+      descriptors.args.push(params[i]);
+    }
+  }
 
   if (help) {
     options.push({ long        : 'help'
@@ -72,26 +109,41 @@ exports.parse = function (options, help) {
                  , callback    : exports.help
                  });
   }
-  descriptors = options;
+  for (var i=0; i<options.length; i++) {
+    descriptors.opts.unshift(options[i]);
+  }
+  options = descriptors.opts;
+
+  var checkDup = function (opt, type) {
+    var prefix = (type == 'short')? '-': '--';
+    var name = opt[type];
+    if (!opts[prefix + name]) {
+      opts[prefix + name] = opt;
+    } else {
+      if (opt.namespace && !opts[prefix + opt.namespace + '.' + name]) {
+        opts[prefix + opt.namespace + '.' + name] = opt;
+        for (var i=0; i<descriptors.opts.length; i++) {
+          var desc = descriptors.opts[i];
+          if (desc.namespace == opt.namespace) {
+            if (type == 'long' && desc.long == opt.long) {
+                descriptors.opts[i].long = opt.namespace + '.' + opt.long;
+            } else if (type == 'short') {
+              delete descriptors.opts[i].short;
+            }
+          }
+        }
+      } else {
+        puts('Conflicting flags: ' + prefix + name + '\n');
+        puts(helpString());
+        process.exit(1);
+      }
+    }
+  };
 
   var opts = {};
   for (var i=0; i<options.length; i++) {
-    if (options[i].short) {
-      if (!opts['-' + options[i].short]) {
-        opts['-' + options[i].short] = options[i];
-      } else {
-        puts('Conflicting flags: -' + options[i].short);
-        process.exit(1);
-      }
-    }
-    if (options[i].long) {
-      if (!opts['--' + options[i].long]) {
-        opts['--' + options[i].long] = options[i];
-      } else {
-        puts('Conflicting flags: --' + options[i].long);
-        process.exit(1);
-      }
-    }
+    if (options[i].short) checkDup(options[i], 'short');
+    if (options[i].long) checkDup(options[i], 'long');
   }
 
   for (var i=2; i<process.argv.length; i++) {
@@ -107,7 +159,7 @@ exports.parse = function (options, help) {
         var next = process.argv[i+1];
         if (!next || opts[next]) {
           var flag = opt.short || opt.long;
-          errors.push('Missing value for argument: ' + flag);
+          errors.push('Missing value for option: ' + flag);
           if (opt.short) values[opt.short] = true;
           if (opt.long) values[opt.long] = true;
         } else {
@@ -119,24 +171,35 @@ exports.parse = function (options, help) {
       }
     } else {
       // No match. If it starts with a dash, show an error. Otherwise
-      // add it to the extra args.
+      // add it to the extra params.
       if (inp[0] == '-') {
         puts('Unknown option: ' + inp);
         if (opts['--help']) puts('Try --help');
         process.exit(1);
       } else {
-        args.push(inp);
+        argv.push(inp);
+        var arg = params.shift();
+        if (arg) {
+          args[arg.name] = inp;
+          if (arg.callback) arg.callback(inp);
+        }
       }
     }
   }
   for (var i=0; i<options.length; i++) {
     var flag = options[i].short || options[i].long;
     if (options[i].required && !exports.get(flag)) {
-      errors.push('Missing required argument: ' + flag);
+      errors.push('Missing required option: ' + flag);
+    }
+  }
+  for (var i=0; i<params.length; i++) {
+    if (params[i].required && !args[params[i].name]) {
+      errors.push('Missing required argument: ' + params[i].name);
     }
   }
   if (errors.length) {
     for (var i=0; i<errors.length; i++) puts(errors[i]);
+    puts('\n' + helpString());
     process.exit(1);
   }
 };
@@ -153,21 +216,53 @@ exports.get = function (opt) {
  * Get unknown args. Could have special meaning to client
  */
 exports.args = function () {
-  return args;
+  return argv;
 };
 
+/**
+ * Get an arg by name.
+ * This only works if arg names were passed into the parse function.
+ * @param string name Name of arg
+ * @return string Value of arg
+ */
+exports.arg = function (name) {
+  //puts(require('sys').inspect(arguments));
+  return args[name];
+};
+
+/**
+ * Print the help message and exit
+ */
 exports.help = function () {
-  for (var i=0; i<descriptors.length; i++) {
-    var opt = descriptors[i];
-    var line;
-    if (opt.description) puts(opt.description);
-    if (opt.short && !opt.long) line = '-' + opt.short;
-    else if (opt.long && !opt.short) line = '--' + opt.long;
-    else line = '-' + opt.short + ', --' + opt.long;
-    if (opt.value) line += ' <value>';
-    if (opt.required) line += ' (required)';
-    puts('    ' + line);
-  }
+  puts(helpString());
   process.exit(0);
 };
 
+
+// Create the help string
+var helpString = function () {
+  var str = 'Usage: ' + process.argv[0] + ' ' + process.argv[1];
+  if (descriptors.opts.length) str += ' [options]';
+  if (descriptors.args.length) {
+    for (var i=0; i<descriptors.args.length; i++) {
+      if (descriptors.args[i].required) {
+        str += ' ' + descriptors.args[i].name;
+      } else {
+        str += ' [' + descriptors.args[i].name + ']';
+      }
+    }
+  }
+  str += '\n';
+  for (var i=0; i<descriptors.opts.length; i++) {
+    var opt = descriptors.opts[i];
+    if (opt.description) str += (opt.description) + '\n';
+    var line = '';
+    if (opt.short && !opt.long) line += '-' + opt.short;
+    else if (opt.long && !opt.short) line += '--' + opt.long;
+    else line += '-' + opt.short + ', --' + opt.long;
+    if (opt.value) line += ' <value>';
+    if (opt.required) line += ' (required)';
+    str += '    ' + line + '\n';
+  }
+  return str;
+};
